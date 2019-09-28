74fd6aff993203f4dfc9e1bcea9fa0a331da7156
League Student <league@iMac-12.attlocal.net>
2019-09-28 12:55:59 -0700

    four square and pentagon crazy

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,33 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
+	Robot thanos = new Robot();
+	Robot noob = new Robot();
+	Robot nerd = new Robot();
+	Robot boomer = new Robot();
+	thanos.setSpeed(100);
+	noob.setSpeed(100);
+	nerd.setSpeed(100);
+	boomer.setSpeed(100);
+	thanos.penDown();
+	nerd.penDown();
+	noob.penDown();
+	boomer.penDown();
+	noob.turn(90);
+	nerd.turn(180);
+	boomer.turn(270);
+	thanos.turn(360);
+	noob.move(100);
+	nerd.move(100);
+	boomer.move(100);
+	thanos.move(100);
+	noob.turn(90);
+	nerd.turn(90);
+	boomer.turn(90);
+	thanos.turn(90);
+	noob.move(100);
+	nerd.move(100);
+	boomer.move(100);
+	thanos.move(100);
 	}
 }
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,27 +10,29 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
+Robot fbi = new Robot();
 
 		// 3. Put the robot's pen down
-
+fbi.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+fbi.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+fbi.setPenColor(255,0,255);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
+int sides = 5;
 		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
+int angles = 108;
 		// 7. Use a for loop to repeat the steps below (#2, #10, #6, #11) 200 times. When this is done you should see a pentagon
-
+for(int i=0; i<200; i++) {
 			// 2. Move the robot 200 pixels
-	
+	fbi.move(i);
 			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
 	
 			// 6. Turn the robot the amount in your angle variable
-	
+	fbi.turn(108);
 			// 11. Turn the robot one more degree
-
+fbi.turn(1);
+	}
 	}
 
 	// Variations:
