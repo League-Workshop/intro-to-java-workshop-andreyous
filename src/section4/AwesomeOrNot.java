249ff04f1993e47ac99d523fe629fc4c48d537ee
League Student <league@iMac-12.attlocal.net>
2019-10-12 12:59:53 -0700

    dragon fight

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -5,9 +5,10 @@ import java.util.Random;
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
+public static void main(String[] args) {
 
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+	
 	// 3. Print out this variable
 
 	// 4. Get the user to enter something that they think is awesome
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,20 +1,24 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score=0;
 		// 2.  Ask the user a question 
-		
+		String answer=JOptionPane.showInputDialog("Do you know Joe?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer.equals("Joe Mama"));
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		score++;
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		String answer2=JOptionPane.showInputDialog("Have u ever seen a dragon?");
+		if(answer.equals("nut"));
+		score++;
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "Score: " +score);
 	}
 }
